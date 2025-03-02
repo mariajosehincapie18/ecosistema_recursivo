47d1712d30e30b6f18629bcf0d1164cc2aaa24ec
mariajosehincapie18 <mjhincapie18@gmail.com>
2025-03-02 16:16:12 -0500

    tablero.2

--- tablero.py
+++ tablero.py
@@ -12,14 +12,16 @@ class Tablero:
         return self.generar_tablero(n, i, j+1, fila, matriz)
     
 
-    def agregar_platas_al_tablero(self, matriz):
-        self.matriz = matriz
-
+    def agregar_platas_al_tablero(self, matriz, cantidad_plantas= 4):
         celdas_vacias= [(i,j)for i in range (len(matriz)) for j in range(len(matriz[0])) if matriz [i][j] == "__"] 
 
-        if celdas_vacias:
-            i, j= random.choice(celdas_vacias)
-            matriz [i][j] = "Pl"
+        if  cantidad_plantas == 0 or not celdas_vacias:
+            return matriz
+        
+
+        i, j= random.choice(celdas_vacias)
+        matriz [i][j] = "Pl"
+        return self.agregar_platas_al_tablero(matriz, cantidad_plantas -1)
 
 
 
